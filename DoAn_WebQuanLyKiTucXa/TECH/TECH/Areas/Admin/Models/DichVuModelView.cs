de9b8f3f5023651b4d7a60695d886f934b36375d
VanCanh1102 <nvc11022003@gmail.com>
2025-04-03 15:34:19 +0700

    update 3/4/2025

--- DoAn_WebQuanLyKiTucXa/TECH/TECH/Areas/Admin/Models/DichVuModelView.cs
+++ DoAn_WebQuanLyKiTucXa/TECH/TECH/Areas/Admin/Models/DichVuModelView.cs
@@ -1,11 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace TECH.Areas.Admin.Models
+﻿namespace TECH.Areas.Admin.Models
 {
     public class DichVuModelView
     {
--- DoAn_WebQuanLyKiTucXa/TECH/TECH/Areas/Admin/Models/Search/DichVuViewModelSearch.cs
+++ DoAn_WebQuanLyKiTucXa/TECH/TECH/Areas/Admin/Models/Search/DichVuViewModelSearch.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace TECH.Areas.Admin.Models.Search
+﻿namespace TECH.Areas.Admin.Models.Search
 {
     public class DichVuViewModelSearch : PageViewModel
     {
